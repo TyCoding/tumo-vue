63733d446e90ad1c8d91ddcd6d3d1b6253f90ff1
TyCoding <2557988481@qq.com>
2019-01-01 14:39:34 +0800

    :smile: 完善项目

--- vue/config/prod.env.js
+++ vue/config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+    NODE_ENV: '"production"',
+    ENV_CONFIG: '"prod"',
+    BASE_API: '"http://127.0.0.1:8084/"',
 }
