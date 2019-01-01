632f86cecbc3e762a96da1f02c201a596fa91c16
TyCoding <2557988481@qq.com>
2019-01-01 21:01:12 +0800

    :smile: 项目部署

--- api/src/main/java/cn/tycoding/ApiApplication.java
+++ api/src/main/java/cn/tycoding/ApiApplication.java
@@ -2,11 +2,18 @@ package cn.tycoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
--- vue/config/prod.env.js
+++ vue/config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"prod"',
-    BASE_API: '"http://127.0.0.1:8084/"',
+    BASE_API: '"http://ip"',
 }
