0e28b61aa1d20f638a03eb4461e7b3aa8b905bc6
tycoding <tycoding@tycodingdeMBP.lan>
2020-07-28 09:12:46 +0800

    改用webpack代理解决跨域问题

--- app/vue.config.js
+++ app/vue.config.js
@@ -35,15 +35,15 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    proxy: {
+      // change xxx-api/login => mock/login
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      '/': {
+        target: `http://localhost:8080/api`,
+        ws: true
+      }
     }
-    // proxy: {
-    //   // change xxx-api/login => mock/login
-    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
-    //   [process.env.VUE_APP_BASE_API]: {
-    //     target: `http://localhost:8080`,
-    //     changeOrigin: true
-    //   }
-    // }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
